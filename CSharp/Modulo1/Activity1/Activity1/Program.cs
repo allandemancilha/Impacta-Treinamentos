c14b7cc6967f329ddae835d9537fce3555a2b140
allandemancilha <allandemancilha@gmail.com>
2021-01-29 10:11:07 -0300

    Adicionando Aplicativo Console Activity1

---
+++ CSharp/Modulo1/Activity1/Activity1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Activity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nome: Allan Mancilha");
+            Console.WriteLine("Email: allanxxxxxxxxxx@email.com");
+            Console.WriteLine("Telefone: (11) XXXXX-XXXX");
+
+            Console.ReadLine();
+        }
+    }
+}
