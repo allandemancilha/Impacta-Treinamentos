e467f5c52b7c16024a49f1189b3b0f3f94f6fa6f
allandemancilha <allandemancilha@gmail.com>
2021-02-02 18:09:05 -0300

    Adicionando Aplicativo Console Lesson5.Activity1

---
+++ CSharp/Modulo1/Lesson5.Activity1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson5.Activity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o Valor do Produto: R$ ");
+            decimal productPrice = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Digite a Quantidade: ");
+            int productQuantity = Convert.ToInt32(Console.ReadLine());
+
+            decimal totalPurchasePrice = productPrice * productQuantity;
+
+            Console.WriteLine("Preço Total da Compra: {0:c}", totalPurchasePrice);
+            Console.ReadLine();
+        }
+    }
+}
+
