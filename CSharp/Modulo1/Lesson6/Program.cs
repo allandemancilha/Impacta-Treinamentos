2804e528ab2e5c210aab8a7bb23b12024f2e3db5
allandemancilha <allandemancilha@gmail.com>
2021-02-05 16:39:30 -0300

    Estrutura de Decisão Simples (If), Composta (If, Else) e Aninhada (If, Else If, Else)

---
+++ CSharp/Modulo1/Lesson6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um Número Entre 1-10: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number < 1 || number > 10)
+            {
+                Console.WriteLine("Número Fora da Faixa.");
+            }
+            else
+            {
+                Console.WriteLine("Obrigado!");
+            }
+
+            Console.WriteLine("Aperte Alguma Tecla Para Continuar...");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Dia, Tarde ou Noite?");
+            Console.WriteLine();
+
+            int hour = DateTime.Now.Hour;
+
+            if (hour > 17)
+            {
+                Console.WriteLine("Boa Noite!");
+            } 
+            else if (hour > 11)
+            {
+                Console.WriteLine("Boa Tarde!");
+            } 
+            else
+            {
+                Console.WriteLine("Bom Dia!");
+            }
+
+            Console.WriteLine("Aperte Alguma Tecla Para Encerrar...");
+            Console.ReadLine();
+        }
+    }
+}
