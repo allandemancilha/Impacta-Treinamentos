5aae2c5119c435207f77561e28dfc7bbc0c77b2b
allandemancilha <allandemancilha@gmail.com>
2021-02-05 20:03:14 -0300

    Adicionando Aplicativo Console Lesson6.Activity1

---
+++ CSharp/Modulo1/Lesson6.Activity1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson6.Activity1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o Valor da Compra: R$ ");
+            decimal purchasePrice = Convert.ToDecimal(Console.ReadLine());
+            decimal priceMinimumToParcels = 1000.01m;
+
+            Console.WriteLine("Valor à Vista: {0:c}", purchasePrice);
+
+            if (purchasePrice >= priceMinimumToParcels) 
+            {
+                int installmentsNumber = 3;
+                decimal installmentsPrice = Convert.ToDecimal(purchasePrice / installmentsNumber);
+
+                Console.WriteLine("{0}x de {1:c}", installmentsNumber, installmentsPrice);
+            }
+
+            Console.WriteLine("Tecle ENTER Para Finalizar");
+            Console.ReadLine();
+
+        }
+    }
+}
